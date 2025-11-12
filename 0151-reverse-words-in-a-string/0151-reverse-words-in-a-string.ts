18a24cc8b8a1ddf765018140b19c6937ecee70b6
pjoywaves <pjoywaves@gmail.com>
2025-11-12 13:14:50 +0900

    Time: 1 ms (81.51%), Space: 57.8 MB (39.27%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
@@ -1,7 +1,11 @@
 function reverseWords(s: string): string {
-  return s
-    .split(" ")
-    .filter((s) => s !== "")
-    .reverse()
-    .join(" ");
+    // s를 " "을 기준으로 나눈다.
+    // 나눠진 값중 실제 데이터가 들어있는 값만 filter 처리한다.
+    // 배열 반전을 한다
+    // 다시 " "로 join 하여 결과를 도출한다.
+    return s
+        .split(" ")
+        .filter((s) => s !== "")
+        .reverse()
+        .join(" ");
 };
