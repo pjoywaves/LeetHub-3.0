321bb540635d00f76706c6237e7adb504783798f
pjoywaves <pjoywaves@gmail.com>
2025-11-12 13:04:25 +0900

    Time: 0 ms (100%), Space: 56.4 MB (56.33%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.ts
@@ -1,6 +1,10 @@
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-  let maxValue = Math.max(...candies);
+    // candies 에 담긴 값 중 가장 큰 값을 찾는다.
+    let maxValue = Math.max(...candies);
 
-  let result = candies.map((num) => num + extraCandies >= maxValue);
-  return result;
+    // candies 을 확인하여 요소(kid)가 extraCandies의 값을 추가적으로 가졌을때
+    // 본래 candies의 가장 큰 값보다 크거나 같은지 비교하여 결과값을 도출해낸다.
+    let result = candies.map((num) => num + extraCandies >= maxValue);
+
+    return result;
 };
