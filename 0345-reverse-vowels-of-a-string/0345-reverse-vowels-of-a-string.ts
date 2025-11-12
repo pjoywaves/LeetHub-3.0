c2ca772f93dc544d64279390f5c2334bde08b717
pjoywaves <pjoywaves@gmail.com>
2025-11-12 13:13:19 +0900

    Time: 20 ms (19.01%), Space: 67.3 MB (5.67%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.ts
@@ -1,32 +1,43 @@
+// value와 index 객체 타입
 type getVowels = {
   value: string;
   index: number;
 };
 
 function reverseVowels(s: string): string {
-  let arr: string[] = s.split("");
-  let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-  let findVowels: getVowels[] = [];
+    // s 를 나누어 저장할 배열 변수
+    let arr: string[] = s.split("");
 
-  for (let i = 0; i < arr.length; i++) {
-    if (vowels.includes(arr[i])) {
-      findVowels.push({
-        value: arr[i],
-        index: i,
-      });
+    // 모음 정의
+    let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+    let findVowels: getVowels[] = [];
+
+    // arr을 도는 동안
+    // 정의된 모음값에 arr[i]이 포함되면
+    // 특정 타입으로 정의된 findVowls 에 해당 값과 index(i)값을 저장한다.
+    for (let i = 0; i < arr.length; i++) {
+        if (vowels.includes(arr[i])) {
+        findVowels.push({
+            value: arr[i],
+            index: i,
+        });
+        }
     }
-  }
 
-  let modifiedVowls: getVowels[] = findVowels.map((v) => ({ ...v }));
-  for (let i = 0; i < findVowels.length; i++) {
-    let left = findVowels[i];
-    let right = findVowels[findVowels.length - (1 + i * 1)];
-    modifiedVowls[i].index = right.index;
-    modifiedVowls[findVowels.length - (1 + i * 1)].index = left.index;
-  }
+    // arr 에서 찾은 모음들을 순회하며
+    // 양끝부터 가운데로 차례대로 자리(index)를 바꾼다.
+    let modifiedVowls: getVowels[] = findVowels.map((v) => ({ ...v }));
+    for (let i = 0; i < findVowels.length; i++) {
+        let left = findVowels[i];
+        let right = findVowels[findVowels.length - (1 + i * 1)];
+        modifiedVowls[i].index = right.index;
+        modifiedVowls[findVowels.length - (1 + i * 1)].index = left.index;
+    }
 
-  for (let i = 0; i < modifiedVowls.length; i++) {
-    arr[modifiedVowls[i].index] = modifiedVowls[i].value;
-  }
-  return arr.join("");
+    // 바뀐 index 값에 맞게 arr에 다시 넣고 string으로 join 하여 결과를 도출한다.
+    for (let i = 0; i < modifiedVowls.length; i++) {
+        arr[modifiedVowls[i].index] = modifiedVowls[i].value;
+    }
+    return arr.join("");
 }
