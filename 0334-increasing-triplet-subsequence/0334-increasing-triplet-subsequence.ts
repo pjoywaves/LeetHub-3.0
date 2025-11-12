f81a7c6f8be99c9efd8f87bc0a50146c0e358b7b
pjoywaves <pjoywaves@gmail.com>
2025-11-12 14:36:14 +0900

    Time: 5 ms (27.58%), Space: 75.2 MB (45.74%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.ts
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.ts
@@ -1,12 +1,21 @@
 function increasingTriplet(nums: number[]): boolean {
-  let first = Infinity;
-let second = Infinity;
+    // 처음 큰 값, 다음 큰 값을 담을 변수를 만들었다.
+    let first: number = Infinity;
+    let second: number = Infinity;
 
-for (const n of nums) {
-  if (n <= first) first = n;
-  else if (n <= second) second = n;
-  else return true; // n > second > first
+    /**
+        nums의 요소들이 순회되면서
+        first, second에 각각 작은 값들이 담기고
+        i < j < k 의 조건을 만족하게 되면 true를 리턴하게 된다.
+     */
+    for (let n of nums) {
+        if (n <= first) {
+            first = n;
+        } else if (n <= second) {
+            second = n;
+        } else {
+            return true;
+        }
+    }
+    return false;
 }
-
-return false;
-};
