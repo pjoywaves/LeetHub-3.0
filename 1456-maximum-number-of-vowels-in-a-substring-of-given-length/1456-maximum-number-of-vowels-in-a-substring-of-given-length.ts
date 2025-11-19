fbea8639cc73fd2793fc20d702b0ca1004b1492e
pjoywaves <pjoywaves@gmail.com>
2025-11-19 17:01:08 +0900

    Time: 21 ms (30.82%), Space: 58.3 MB (67.89%) - LeetHub

---
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.ts
@@ -0,0 +1,18 @@
+function maxVowels(s: string, k: number): number {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count: number = 0;
+
+  for (let i = 0; i < k; i++) {
+    if (vowels.includes(s[i])) count++;
+  }
+
+  let max: number = count;
+
+  for (let i = 1; i < s.length; i++) {
+    if (vowels.includes(s[i - 1])) count--;
+    if (vowels.includes(s[i + k - 1])) count++;
+    max = Math.max(max, count);
+  }
+
+  return max;
+};
