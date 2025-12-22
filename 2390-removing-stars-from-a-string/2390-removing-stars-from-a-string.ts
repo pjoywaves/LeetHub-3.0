a588cc13b0bac59a23490edbff658c9177131afd
pjoywaves <pjoywaves@gmail.com>
2025-12-22 15:49:37 +0900

    Time: 18 ms (86.61%), Space: 70 MB (62.96%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.ts
@@ -0,0 +1,12 @@
+function removeStars(s: string): string {
+  let result: string[] = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== '*') {
+      result.push(s[i]);
+    } else {
+      result.pop();
+    }
+  }
+  return result.join("");
+};
